ff4278925637673ed6af4ec5fe01ef8b277bbc70
mhmda <mhmdaljzar036@gmail.com>
2025-05-14 23:30:54 +0300

    finished doctor carousel in home page

--- Frontend/assets/js/api/doctors.js
+++ Frontend/assets/js/api/doctors.js
@@ -1,46 +1,46 @@
-import { request, TOKEN } from "./shared.js";
-async function getAllDoctors(TOKEN) {
+import { request } from "./shared.js";
+export async function getAllDoctors(TOKEN) {
 	return await request(`/api/Doctor`, TOKEN);
 }
-async function getDoctorById(id, TOKEN) {
+export async function getDoctorById(id, TOKEN) {
 	return await request(`/api/Doctor/${id}`, TOKEN);
 }
-async function getDoctorPatients(id, TOKEN) {
+export async function getDoctorPatients(id, TOKEN) {
 	return await request(`/api/Doctor/${id}/patients`, TOKEN);
 }
-async function filterDoctors(queryOptions, TOKEN) {
+export async function filterDoctors(queryOptions, TOKEN) {
 	const query = new URLSearchParams(queryOptions).toString();
 	return await request(`/api/Doctor?${query}`, TOKEN);
 }
-async function getDoctorAppointments(id, TOKEN) {
+export async function getDoctorAppointments(id, TOKEN) {
 	return await request(`/api/Doctor/${id}/appointments`, TOKEN);
 }
-async function getDoctorUpcomingAppointments(id, TOKEN) {
+export async function getDoctorUpcomingAppointments(id, TOKEN) {
 	return await request(`/api/Doctor/${id}/upcomingAppointments`, TOKEN);
 }
-async function getDoctorAverageRating(id, TOKEN) {
+export async function getDoctorAverageRating(id, TOKEN) {
 	return await request(`/api/Doctor/${id}/rating`, TOKEN);
 }
-async function getDoctorRatingByPatient(id, patId, TOKEN) {
+export async function getDoctorRatingByPatient(id, patId, TOKEN) {
 	return await request(`/api/Doctor/${id}/rating/${patId}`, TOKEN);
 }
-async function getDoctorProfitByDate(id, TOKEN, date) {
+export async function getDoctorProfitByDate(id, TOKEN, date) {
 	return await request(`/api/Doctor/${id}/profit/${date}`, TOKEN);
 }
 
-async function addDoctor(TOKEN, doctor) {
+export async function addDoctor(TOKEN, doctor) {
 	return await request(`/api/Doctor`, TOKEN, {
 		method: "POST",
 		body: doctor,
 	});
 }
-async function updateDoctorById(id,doctor,TOKEN) {
+export async function updateDoctorById(id,doctor,TOKEN) {
 	return await request(`/api/Doctor`, TOKEN, {
 		method: "PUT",
 		body: doctor,
 	});
 }
-async function deleteDoctorById(id,TOKEN) {
+export async function deleteDoctorById(id,TOKEN) {
 	return await request(`/api/Doctor`, TOKEN, {
 		method: "DELETE",
 	});
