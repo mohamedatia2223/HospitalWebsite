5dce9f13fe37b5b30ac353ee07b42d6f5ab5d0ff
Mohamed Atia <mohamedatia2223@gmail.com>
2025-05-15 13:45:15 +0300

    Add files via upload

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace Hospital
 {
     public class Program
@@ -53,6 +55,34 @@ namespace Hospital
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+                        });
 
             var app = builder.Build();
 
