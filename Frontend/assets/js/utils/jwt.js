8ec480a1f96941f813c479f70088443ece63dde5
mhmda <mhmdaljzar036@gmail.com>
2025-05-20 16:00:13 +0300

    dynamically redireting user to their respective dashboards

--- Frontend/assets/js/utils/jwt.js
+++ Frontend/assets/js/utils/jwt.js
@@ -16,9 +16,11 @@ export const logOut = () => {
 };
 
 export const parseJwt = (token) => {
+	console.log(token)
 	try {
 		return JSON.parse(atob(token.split(".")[1]));
 	} catch (e) {
+		console.log(e);
 		return null;
 	}
 };
